8b65aab3f4a95722e911cef0786a47ab482d244d
Arnon-T <arnon.hts@gmail.com>
2019-09-13 16:30:40 -0300

    Tela salvar funcional

--- src/main/java/br/com/proway/api/data/ConexaoMysqlJDBC.java
+++ src/main/java/br/com/proway/api/data/ConexaoMysqlJDBC.java
@@ -13,10 +13,10 @@ public class ConexaoMysqlJDBC implements ConexaoJDBC {
 	private static final String USERNAME = "root";
 
 	// Senha do mysql
-	private static final String PASSWORD = "123456";
+	private static final String PASSWORD = "root";
 
 	// Dados de caminho, porta e nome da base de dados que irá ser feita a conexão
-	private static final String DATABASE_URL = "jdbc:mysql://0.0.0.0:8888/exemplo_jdbc?useTimezone=true&serverTimezone=UTC";
+	private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/exemplo_jdbc?useTimezone=true&serverTimezone=UTC";
 
 	public ConexaoMysqlJDBC() throws SQLException, ClassNotFoundException {
 		Class.forName("com.mysql.jdbc.Driver"); 
